0c7d87973bea8adf5ee8b57d6db7886bb40e0ebf
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-07-16 12:02:42 -0400

    wip

--- data/migrations/20190716085146_createEmailsTable.js
+++ data/migrations/20190716085146_createEmailsTable.js
@@ -2,9 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('emails', table => {
     table.increments();
     table.text('email', 128).unique().notNullable();
-    table.integer('userId')
-    //   .notNullable();
-    // table.foreign('userId').references('users.id');
+    table.integer('userId');
   });
 };
 
